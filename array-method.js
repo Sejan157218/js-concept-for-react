0e8fcb108fe34aeaf318c5b24b39a3e898a93d7b
sejan-mahmud <sejan157218@gmail.com>
2021-09-11 15:28:01 +0600

    array-method and some method des

---
+++ array-method.js
@@ -0,0 +1,36 @@
+const arrayMethod = ['sejan',1,2,3,55,66,77,88,'sajal']
+
+// map in array 
+const mapInArray = arrayMethod.map(element=>element*2)
+console.log(mapInArray);
+const findInArray = arrayMethod.find(element=>element===66)
+console.log(findInArray);
+const filterInArray = arrayMethod.filter(element=>element<60)
+console.log(filterInArray);
+
+// map, forEach, filter, find
+/* 
+map = kuno akta array theke element nia kisu kaj kore abr result array hishebe return kore 
+
+forEach = kuno akta array theke element nia kisu kaj kore but return kore na...console.log kore and function k call kore
+
+filter = je element gulo condition mane sei element k bad diye new array create kore return kore
+
+find = je element condition mane sei element k return kore
+*/
+
+/* ++ = 1 plus kore, 
+-- = 1 minuse kore, 
++ =string name ar age dile number a convart kore, 
++'' = number k string a convart kore, 
++= =sei element ar sathe plus kore, 
+-= = sei element ar sathe mibuse kore,
+active = !active  = ( true  = fase ) true k false kore
+
+*/
+
+
+/* JSON 
+stringify = string a convart kore 
+parse = normal js arry ba object a convart kore
+ */
